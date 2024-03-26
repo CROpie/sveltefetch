25bc0641133e95ac30aa5bd55a95b63929808711
CROpie <96667010+CROpie@users.noreply.github.com>
2024-03-27 10:28:04 +1100

    Added fetch using +page.server.js

---
+++ src/routes/+page.server.js
@@ -0,0 +1,23 @@
+import { json } from '@sveltejs/kit'
+
+export async function load() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+  // ok: true or false
+  // status: 200 (success), 404 (not found) etc
+  // where is the data? body: ReadableStream
+  console.log(response)
+
+  // eg if 404, 500
+  if (!response.ok) {
+    console.log("couldn't get the data..")
+    return { success: false }
+  }
+
+  // use the .json() method to read body: ReadableStream and parse it as JSON
+  const myData = await response.json()
+
+  console.log(myData)
+
+  return { success: true, fetchedData: myData }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,10 @@
 <script>
-async function getData() {
-
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-
-    // ok: true or false
-    // status: 200 (success), 404 (not found) etc
-    // where is the data? body: ReadableStream
-    console.log(response)
-
-    // eg if 404, 500
-    if (!response.ok) {
-      console.log("couldn't get the data..")
-      return
-    }
-
-    // use the .json() method to read body: ReadableStream and parse it as JSON
-    const data = await response.json()
-
-    console.log(data)
-    
-    return data
-  }
-
-
-getData()
+  export let data
+  console.log("+page.svelte: ", data)
 </script>
 
-{#await getData()}
-<p>Loading...</p>
-{:then data}
-
-{#each data as item}
-<p>{item.name}</p>
-{/each}
-{/await}
+<ul>
+  {#each data.fetchedData as item}
+  <li>{item.name}</li>
+  {/each}
+</ul>
