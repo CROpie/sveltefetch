543dd3905e0365a78ce35a6e52e4ecbc4f352419
CROpie <96667010+CROpie@users.noreply.github.com>
2024-03-27 11:16:31 +1100

    Better error handling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,26 @@
 <script>
 async function getData() {
 
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const response = await fetch('https://jsonplaceholder.typicode.com/userszzz')
 
     // ok: true or false
     // status: 200 (success), 404 (not found) etc
     // where is the data? body: ReadableStream
-    console.log(response)
+    // console.log(response)
 
     // eg if 404, 500
     if (!response.ok) {
       console.log("couldn't get the data..")
-      return
+      throw new Error
     }
 
     // use the .json() method to read body: ReadableStream and parse it as JSON
     const data = await response.json()
 
-    console.log(data)
+    // console.log(data)
     
     return data
   }
-
-
-getData()
 </script>
 
 <h3>Testing out Fetch API</h3>
@@ -34,4 +31,7 @@ getData()
 {#each data as item}
 <p>{item.name}</p>
 {/each}
+
+{:catch}
+<p>There was a problem getting data from the database</p>
 {/await}
